cfb7645fd5d736a67d2d7522820017022290b086
yuk228 <joyful.nachos-0w@icloud.com>
2025-04-26 17:17:11 +0900

    ✨ Add terms and privacy policy

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <main className="flex flex-col items-center justify-center h-screen">
+            <h1>Dashboard</h1>
+        </main>
+    )
+}
--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { SignedIn, UserButton } from "@clerk/nextjs"
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,7 +8,12 @@ export default async function Header () {
     <header className="fixed top-0 w-full z-50 backdrop-blur-sm">
         <div className="container mx-auto px-4 h-16 flex items-center justify-between">
             <Link href="/" className="block font-bold text-xl transition-transform duration-300 ease-in-out transform hover:scale-105">DeltaVerify</Link>
-            <Link href="/dashboard"><Button>Dashboard</Button></Link>
+            <div className="flex items-center gap-4">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+              <Link href="/dashboard"><Button>Dashboard</Button></Link>
+            </div>
         </div>
     </header>
   )
