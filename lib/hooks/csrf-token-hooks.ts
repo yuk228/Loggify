b6afc9e4f301e13fd599c5e1958187b7e132084b
Yuk <194586153+yuk228@users.noreply.github.com>
2025-07-21 21:00:12 +0900

    🚀 Remove webpack to fix deploy errors

--- lib/hooks/csrf-token-hooks.ts
+++ lib/hooks/csrf-token-hooks.ts
@@ -25,6 +25,6 @@ export function useCsrfToken() {
       }
     };
     fetchCsrfToken();
-  }, []);
+  }, [router]);
   return csrfToken;
 }
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.node/,
-      use: "node-loader",
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
